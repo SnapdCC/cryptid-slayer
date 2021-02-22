10eedc07761e1e63f744fa17b2c47e3f83307e8f
muhammadn1200 <79211720+muhammadn1200@users.noreply.github.com>
2021-02-22 10:43:31 -0600

    added  puzzle mechanic

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Pickup : MonoBehaviour
 {
+    int count;
+    GameObject[] gameObjects;
+    public GameObject border;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,10 @@ public class Pickup : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (count == 4)
+        {
+            DestroyAllRocks();
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -21,6 +27,18 @@ public class Pickup : MonoBehaviour
         if (collision.gameObject.CompareTag("item"))
         {
             collision.gameObject.SetActive(false);
+            count++;
         }
+    }
+
+    void DestroyAllRocks()
+    {
+        gameObjects = GameObject.FindGameObjectsWithTag("rock");
+
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            Destroy(gameObjects[i]);
+        }
+        Destroy(border);
     }
 }
