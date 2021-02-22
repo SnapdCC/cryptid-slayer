f32538b5028f050bcdaee29218bf3ea6c989e5a8
muhammadn1200 <79211720+muhammadn1200@users.noreply.github.com>
2021-02-21 20:47:51 -0600

    Added newest character sprites and animations
    Also,
    
    Added borders to map
    New area to tile map
    Clue Markers

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,37 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
 
+    void Start()
+    {
+        animator.SetBool("WalkingF", false);
+        animator.SetBool("WalkingB", false);
+    }
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            animator.SetBool("WalkingB", true);
+        }
+        else
+            animator.SetBool("WalkingB", false);
+            
+
+        if (Input.GetKey(KeyCode.S) == true)
+        {
+            animator.SetBool("WalkingF", true);
+        }
+        else
+            animator.SetBool("WalkingF", false);
+
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
     }
 
     void FixedUpdate()
