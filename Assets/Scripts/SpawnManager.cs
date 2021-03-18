96b9b6523532fa2397f68bc3c39bb9414e328fc7
muhammadn1200 <79211720+muhammadn1200@users.noreply.github.com>
2021-03-17 22:24:21 -0500

    hmm

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,7 @@ public class SpawnManager : MonoBehaviour
    public List<GameObject> possibleSpawnPoints = new List<GameObject>();
    public GameObject jackalopePrefab;
    private int chosenSpawnPoint;
+   private int prevSpawnPoint;
 
 
 
@@ -22,8 +23,20 @@ public class SpawnManager : MonoBehaviour
 
     public void newspawn()
     {
-        jackalopePrefab.transform.position = (possibleSpawnPoints[chosenSpawnPoint].transform.position);
-        chosenSpawnPoint = Random.Range(0, possibleSpawnPoints.Count);
+        
+        if (prevSpawnPoint == chosenSpawnPoint && possibleSpawnPoints.Count > 1)
+        {
+            prevSpawnPoint = chosenSpawnPoint;
+            chosenSpawnPoint = Random.Range(0, possibleSpawnPoints.Count);
+            jackalopePrefab.transform.position = (possibleSpawnPoints[chosenSpawnPoint].transform.position);
+
+        }
+        if (prevSpawnPoint != chosenSpawnPoint)
+        {
+            chosenSpawnPoint = Random.Range(0, possibleSpawnPoints.Count);
+            jackalopePrefab.transform.position = (possibleSpawnPoints[chosenSpawnPoint].transform.position);
+        }
+        
     }
          
      
