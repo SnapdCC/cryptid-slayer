288f988a0889e99683827876b932fd5bf7b98394
muhammadn1200 <79211720+muhammadn1200@users.noreply.github.com>
2021-03-22 16:46:17 -0500

    update

---
+++ Assets/JackalopeStopping.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JackalopeStopping : MonoBehaviour
+{
+    public GameObject spawnmanager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    void OnTriggerStay2D(Collider2D trigger)
+    {
+        Debug.Log("Got caught");
+        Debug.Log(trigger.gameObject.tag);
+        if (trigger.gameObject.CompareTag("trap"))
+        {
+            Debug.Log("Does if work");
+            trigger.GetComponent<Trap>().Shut();
+            spawnmanager.GetComponent<SpawnManager>().enabled = false;
+        }
+    }
+}
