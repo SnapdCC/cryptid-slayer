5bc6e5bda304581024c5c1c3275290991036844d
muhammadn1200 <79211720+muhammadn1200@users.noreply.github.com>
2021-03-31 10:42:51 -0500

    Added message for Jackalope Foot

---
+++ Assets/Scripts/AbilityManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityManager : MonoBehaviour
+{
+    public GameObject FootPanel;
+
+    void Start()
+    {
+        FootPanel.SetActive(true);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            FootPanel.SetActive(false);
+        }
+    }
+}
