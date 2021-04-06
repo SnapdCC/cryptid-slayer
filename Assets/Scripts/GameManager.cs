76d2e2ef3691908b3ce818c2a1c6d27cf9a2c84f
Bryce <nilesb9960@my.uwstout.edu>
2021-04-06 17:39:24 -0500

    Added Game Manager
    Puts a Star over levels that have been completed on the level select. Doesn't update when you beat a level bc I couldn't find the scripts that make you beat the levels.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    bool ChupaLevelBeaten;
+    bool JackLevelBeaten;
+    bool SquatchLevelBeaten;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ChupaLevelBeaten = true;
+        JackLevelBeaten = false;
+        SquatchLevelBeaten = false;
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void BeatChup(){
+        //Run this when you beat the level
+        ChupaLevelBeaten = true;
+    }
+    public bool ChupIsBeat(){
+        return ChupaLevelBeaten;
+    }
+    public void BeatJack(){
+        //Run this when you beat the level
+        JackLevelBeaten = true;
+    }
+    public bool JackIsBeat(){
+        return JackLevelBeaten;
+    }
+    public void BeatSquatch(){
+        //Run this when you beat the level
+        SquatchLevelBeaten = true;
+    }
+    public bool SquatchIsBeat(){
+        return SquatchLevelBeaten;
+    }
+    public void GameOver(){
+        //Run this when you lose the game or start a new game
+        JackLevelBeaten = false;
+        ChupaLevelBeaten = false;
+        SquatchLevelBeaten = false;
     }
 }
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    GameManager GameManager;
+    [SerializeField]
+    private GameObject ChupBeaten;
+    [SerializeField]
+    private GameObject JackBeaten;
+    [SerializeField]
+    private GameObject SquatchBeaten;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        // Debug.Log(GameManager.gameObject.scene);
+        ChupBeaten.SetActive(GameManager.ChupIsBeat());
+        JackBeaten.SetActive(GameManager.JackIsBeat());
+        SquatchBeaten.SetActive(GameManager.SquatchIsBeat());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
