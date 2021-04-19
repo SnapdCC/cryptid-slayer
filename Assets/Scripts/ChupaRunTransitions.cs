160e7d5146540ebd8d558b3e6ef78242d570bb03
dustytheaverage <andersond7108@my.uwstout.edu>
2021-04-19 01:45:23 -0500

    Chupa Animation Update
    Imported chupa animations and implemented correct strafing behavior. Pouncing still needs touchup

---
+++ Assets/Scripts/ChupaRunTransitions.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChupaRunTransitions : StateMachineBehaviour
+{
+    private SpriteRenderer chupaSprite;
+
+     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        chupaSprite = animator.gameObject.GetComponent<SpriteRenderer>(); 
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        float rotation = animator.GetFloat("rotation");
+
+        //Play the right animation based on the chupa's rotation as well as whether it's currently running clockwise
+        
+        //running down
+        if((rotation > 315f || rotation > 0f && rotation < 45f) && !animator.GetBool("runningClockwise") || (rotation > 135f && rotation < 225f) && animator.GetBool("runningClockwise"))
+        {
+            chupaSprite.flipX = false;
+            animator.Play("Base Layer.ChupaRunDown");
+        }
+        //running up
+        else if ((rotation > 315f || rotation > 0f && rotation < 45f) && animator.GetBool("runningClockwise") || (rotation > 135f && rotation < 225f) && !animator.GetBool("runningClockwise"))
+        {
+            chupaSprite.flipX = false;
+            animator.Play("Base Layer.ChupaRunUp");
+        }
+        //running right
+        else if((rotation > 45f && rotation < 135f) && !animator.GetBool("runningClockwise") || (rotation > 225f && rotation < 315f) && animator.GetBool("runningClockwise"))
+        {
+            chupaSprite.flipX = false;
+            animator.Play("Base Layer.ChupaLeftRightRun");
+        }
+        //running left
+        else if ((rotation > 45f && rotation < 135f) && animator.GetBool("runningClockwise") || (rotation > 225f && rotation < 315f) && !animator.GetBool("runningClockwise"))
+        {
+            chupaSprite.flipX = true;
+            animator.Play("Base Layer.ChupaLeftRightRun");
+        }
+
+
+
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
